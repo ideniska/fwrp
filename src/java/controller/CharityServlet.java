0ae2c41273fcb50f6494c859e0e350e76121acaa
DmlopezC <dmlopez.cac@gmail.com>
2024-07-20 19:03:17 -0400

    Added the view for charity page, the bussiness logic, view and claim items methods for the charity servlet.

--- src/java/controller/CharityServlet.java
+++ src/java/controller/CharityServlet.java
@@ -4,10 +4,63 @@
  */
 package controller;
 
+import businesslayer.CharityBusinessLogic;
+import dataAccessLayer.InventoryDaoImpl;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.InventoryDTO;
+
 /**
  *
  * @author denissakhno
  */
-public class CharityServlet {
+
+
+public class CharityServlet extends HttpServlet {
+    private CharityBusinessLogic charityBusinessLogic;
+
+    @Override
+    public void init() {
+        this.charityBusinessLogic = new CharityBusinessLogic();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            List<InventoryDTO> inventoryItems = charityBusinessLogic.getCharityInventory();
+            request.setAttribute("inventoryItems", inventoryItems);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("views/charityView.jsp");
+            dispatcher.forward(request, response);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new ServletException("Error retrieving charity inventory items", e);
+        }
+    }
     
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            if (paramName.startsWith("claimQuantity_")) {
+                int foodId = Integer.parseInt(paramName.substring(14));
+                int claimQuantity = Integer.parseInt(request.getParameter(paramName));
+                if (claimQuantity > 0) {
+                    try {
+                        charityBusinessLogic.claimItem(foodId, claimQuantity);
+                    } catch (SQLException | ClassNotFoundException e) {
+                        throw new ServletException("Error claiming item", e);
+                    }
+                }
+            }
+        }
+        response.sendRedirect("/charityView");
+    }
 }
