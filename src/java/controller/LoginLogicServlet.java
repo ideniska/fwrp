a2b486b577313e67b965dfb2b405920c7699f464
Berkay Tezcaner <berkaykaan34@hotmail.com>
2024-07-20 22:14:34 -0400

    Login and register view (LoginServlet, RegisterServlet), RegisterUserServlet for user registration, and LoginLogicServlet for user type authentication.

---
+++ src/java/controller/LoginLogicServlet.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dataAccessLayer.UserDaoImpl;
+import model.UserDTO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginLogicServlet extends HttpServlet {
+
+    private UserDaoImpl userDao;
+
+    @Override
+    public void init() throws ServletException {
+        userDao = new UserDaoImpl();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDTO user = userDao.authenticateUser(email, password);
+
+        if (user != null) {
+            request.getSession().setAttribute("user", user);
+            switch (user.getUserType()) {
+                case 1:
+                    response.sendRedirect(request.getContextPath() + "/Consumer");
+                    break;
+                case 2:
+                    response.sendRedirect(request.getContextPath() + "/Charity");
+                    break;
+                case 3:
+                    response.sendRedirect(request.getContextPath() + "/Retailer");
+                    break;
+                default:
+                    //TODO add servlet for jsp for cleaner url
+                    response.sendRedirect(request.getContextPath() + "/login");
+                    break;
+            }
+        } else {
+            response.sendRedirect("loginView.jsp?error=invalid");
+        }
+    }
+}
---
+++ src/java/controller/LoginServlet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Forward request to loginView.jsp
+        request.getRequestDispatcher("/loginView.jsp").forward(request, response);
+    }
+}
