d1bec59cd656368cc3cdc8c86d19dd8e4753fff3
wang0944 <wang0944@algonquinlive.com>
2024-07-20 22:25:59 -0400

    create orderView logic,But there are some problems with the link at the moment, I'm not sure if this is correct

--- src/java/dataAccessLayer/UserDaoImpl.java
+++ src/java/dataAccessLayer/UserDaoImpl.java
@@ -1,117 +1,76 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dataAccessLayer;
 
-
-
-import java.util.List;
-import java.util.ArrayList;
-import java.sql.PreparedStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import model.UserDTO;
 
-/**
- *
- * @author denissakhno
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDaoImpl {
 
     public UserDaoImpl() {
     }
 
-    public List<UserDTO> getAllUsers() throws SQLException {
+    public UserDTO getUserById(int userId) throws SQLException, ClassNotFoundException {
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        ArrayList<UserDTO> users = null;
+        UserDTO user = null;
         try {
             con = DataSource.getConnection();
-            pstmt = con.prepareStatement(
-                     "SELECT user_id, first_name, last_name, phone, address, email, password, user_type, location, communication, food_preference, notifications FROM User ORDER BY user_id");
+            pstmt = con.prepareStatement("SELECT user_id, first_name, last_name, phone, address FROM User WHERE user_id = ?");
+            pstmt.setInt(1, userId);
             rs = pstmt.executeQuery();
-            users = new ArrayList<UserDTO>();
-            while (rs.next()) {
-                UserDTO user = new UserDTO();
+            if (rs.next()) {
+                user = new UserDTO();
                 user.setUserId(rs.getInt("user_id"));
                 user.setFirstName(rs.getString("first_name"));
                 user.setLastName(rs.getString("last_name"));
                 user.setPhone(rs.getString("phone"));
                 user.setAddress(rs.getString("address"));
-                user.setEmail(rs.getString("email"));
-                user.setPassword(rs.getString("password"));
-                user.setUserType(rs.getInt("user_type"));
-                user.setLocation(rs.getString("location"));
-                user.setCommunication(rs.getInt("communication"));
-                user.setFoodPreference(rs.getString("food_preference"));
-                user.setNotifications(rs.getInt("notifications"));
-                users.add(user);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
         } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
+            closeResources(rs, pstmt, con);
         }
-        return users;
+        return user;
     }
 
-    public void addUser(UserDTO user) {
+    public void updateUser(UserDTO user) throws SQLException, ClassNotFoundException {
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
             con = DataSource.getConnection();
-            pstmt = con.prepareStatement(
-                    "INSERT INTO User (first_name, last_name, phone, address, email, password, user_type, location, communication, food_preference, notifications) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            pstmt = con.prepareStatement("UPDATE User SET first_name = ?, last_name = ?, phone = ?, address = ? WHERE user_id = ?");
             pstmt.setString(1, user.getFirstName());
             pstmt.setString(2, user.getLastName());
             pstmt.setString(3, user.getPhone());
             pstmt.setString(4, user.getAddress());
-            pstmt.setString(5, user.getEmail());
-            pstmt.setString(6, user.getPassword());
-            pstmt.setInt(7, user.getUserType());
-            pstmt.setString(8, user.getLocation());
-            pstmt.setInt(9, user.getCommunication());
-            pstmt.setString(10, user.getFoodPreference());
-            pstmt.setInt(11, user.getNotifications());
+            pstmt.setInt(5, user.getUserId());
             pstmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
         } finally {
+            closeResources(null, pstmt, con);
+        }
+    }
+
+    private void closeResources(ResultSet rs, PreparedStatement pstmt, Connection con) {
+        if (rs != null) {
             try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
+                rs.close();
             } catch (SQLException ex) {
                 System.out.println(ex.getMessage());
             }
+        }
+        if (pstmt != null) {
             try {
-                if (con != null) {
-                    con.close();
-                }
+                pstmt.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
             } catch (SQLException ex) {
                 System.out.println(ex.getMessage());
             }
