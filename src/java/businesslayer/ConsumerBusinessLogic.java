db0169bc94400f776d82fad21e1e4483b7a60778
wang0944 <wang0944@algonquinlive.com>
2024-07-22 22:59:46 -0400

    Adjust consumer business logic

--- src/java/businesslayer/ConsumerBusinessLogic.java
+++ src/java/businesslayer/ConsumerBusinessLogic.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.sql.SQLException;
 import model.InventoryDTO;
 import dataAccessLayer.InventoryDaoImpl;
+import dataAccessLayer.TransactionItemDaoImpl;
+import dataAccessLayer.UserTransactionDaoImpl;
+import model.TransactionItemDTO;
+import model.UserTransactionDTO;
 
 /**
  * Business logic layer for managing consumer operations.
@@ -13,26 +17,82 @@ import dataAccessLayer.InventoryDaoImpl;
  */
 public class ConsumerBusinessLogic {
     private InventoryDaoImpl inventoryDao = null;
-    
+    private TransactionItemDaoImpl transactionItemDao = null;
+    private UserTransactionDaoImpl userTransactionDao = null;
+
+     /**
+     * Constructor to initialize the data access objects.
+     */
     public ConsumerBusinessLogic() {
         inventoryDao = new InventoryDaoImpl();
+        transactionItemDao = new TransactionItemDaoImpl();
+        userTransactionDao = new UserTransactionDaoImpl();
     }
 
+     /**
+     * Retrieves all inventory items.
+     *
+     * @return a list of InventoryDTO objects
+     * @throws SQLException if a database access error occurs
+     * @throws ClassNotFoundException if the JDBC driver class is not found
+     */
     public List<InventoryDTO> getAllInventory() throws SQLException, ClassNotFoundException {
         return inventoryDao.getAllInventory();
     }
 
+    /**
+     * Retrieves filtered inventory items based on specific criteria.
+     *
+     * @return a list of InventoryDTO objects
+     * @throws SQLException if a database access error occurs
+     * @throws ClassNotFoundException if the JDBC driver class is not found
+     */
     public List<InventoryDTO> getFilteredInventory() throws SQLException, ClassNotFoundException {
         return inventoryDao.getFilteredInventory();
     }
 
-    
-    public boolean updateInventory(InventoryDTO inventory) throws SQLException, ClassNotFoundException {
-        // TODO YUCHEN: UPDATE THIS METHOD TO ACCEPT an object of InventoryDTO 
-        return inventoryDao.updateInventory(InventoryDTO inventory);
+     /**
+     * Retrieves an inventory item by its ID.
+     *
+     * @param foodId the ID of the inventory item
+     * @return the InventoryDTO object representing the inventory item
+     * @throws SQLException if a database access error occurs
+     * @throws ClassNotFoundException if the JDBC driver class is not found
+     */
+    public InventoryDTO getInventoryById(int foodId) throws SQLException, ClassNotFoundException {
+        return inventoryDao.getInventoryById(foodId);
     }
 
-    public void logPurchase(int userId, int foodId, int quantity, double price) throws SQLException, ClassNotFoundException {
-        inventoryDao.logPurchase(userId, foodId, quantity, price);
+      /**
+     * Updates an existing inventory item.
+     *
+     * @param inventoryItem the InventoryDTO object containing updated information
+     * @throws SQLException if a database access error occurs
+     * @throws ClassNotFoundException if the JDBC driver class is not found
+     */
+    public void updateInventory(InventoryDTO inventoryItem) throws SQLException, ClassNotFoundException {
+        inventoryDao.updateInventory(inventoryItem);
     }
+
+    /**
+     * Adds a new transaction item.
+     *
+     * @param transactionItem the TransactionItemDTO object to be added
+     * @throws SQLException if a database access error occurs
+     * @throws ClassNotFoundException if the JDBC driver class is not found
+     */
+    public void addTransactionItem(TransactionItemDTO transactionItem) throws SQLException, ClassNotFoundException {
+        transactionItemDao.addTransactionItem(transactionItem);
+    }
+
+    /**
+     * Adds a new userTransaction.
+     *
+     * @param userTransaction the UserTransactionDTO object to be added
+     * @throws SQLException if a database access error occurs
+     * @throws ClassNotFoundException if the JDBC driver class is not found
+     */
+    public void addUserTransaction(UserTransactionDTO userTransaction)throws SQLException, ClassNotFoundException {
+        userTransactionDao.addUserTransaction(userTransaction);
+    }   
 }
